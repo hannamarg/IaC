a108a2931ac9a996297cfd2a98a2f5abb6d50902
Hanna Duaas <hmduaas@stud.ntnu.no>
2024-10-09 12:10:17 +0200

    endring sa name

--- uke40/main.tf
+++ uke40/main.tf
@@ -24,7 +24,7 @@ resource "azurerm_resource_group" "hmhd-rg" {
 }
 
 resource "azurerm_storage_account" "sa-demo" {
-  name                     = "hmhddemo"
+  name                     = "hmhddemo222"
   resource_group_name      = azurerm_resource_group.hmhd-rg.name
   location                 = azurerm_resource_group.hmhd-rg.location
   account_tier             = "Standard"
